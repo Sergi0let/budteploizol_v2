692da6ecc6ad36c3acb8ca041012b2ad7e9b6c1d
svash <s.vashkevych@gmail.com>
2025-04-17 16:56:12 +0300

    Add adiniton logs

--- src/hooks/useNovaPoshta.ts
+++ src/hooks/useNovaPoshta.ts
@@ -29,8 +29,9 @@ const useNovaPoshta = () => {
           body: JSON.stringify({ cityName }),
         },
       )
-
+      console.log("Response:", response)
       const data = await response.json()
+      console.log("Data:", data)
       setCities(data || [])
       setShowDropdown(true)
       setWarehouseNumber("")
